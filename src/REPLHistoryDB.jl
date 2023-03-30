e9d60d8325a59d0f17f4424d750dda425249409a
singularitti <singularitti@outlook.com>
2023-03-30 02:15:48 -0400

    Add types `REPLMode`

--- src/REPLHistoryDB.jl
+++ src/REPLHistoryDB.jl
@@ -1,5 +1,12 @@
 module REPLHistoryDB
 
 # Write your package code here.
+abstract type REPLMode end
+struct JuliaMode <: REPLMode end
+struct PkgMode <: REPLMode end
+struct ShellMode <: REPLMode end
+struct CustomMode <: REPLMode
+    mode::String
+end
 
 end
